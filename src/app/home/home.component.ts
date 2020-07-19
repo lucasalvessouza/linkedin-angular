be3d07b69b0b446b7461d9a31a245b17c2f193ad
Lucas Souza <lucas.souza@rockcontent.com>
2020-07-19 18:01:40 -0300

    create card more info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  tags: string[] = ['nodejs', 'angular', 'rxjs', 'react', 'vue']
+
   constructor() { }
 
   ngOnInit(): void {
