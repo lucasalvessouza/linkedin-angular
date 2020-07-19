107f8c492f12ca76f9ee9521bbf7357422a58d62
Lucas Souza <lucas.souza@rockcontent.com>
2020-07-19 09:53:08 -0300

    change header styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'la-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('headerShow', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translate(0px, -20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
+
+  headerState: string = 'ready'
 
   constructor() { }
 
