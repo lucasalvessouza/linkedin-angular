840cd099e32a4d3c41c357c7e5fd398c8a3fab1f
Lucas Alves <lucasalves.s@outlook.com>
2020-08-25 21:50:54 -0300

    fix post content

--- src/app/home/feed/feed.component.ts
+++ src/app/home/feed/feed.component.ts
@@ -12,16 +12,31 @@ export class FeedComponent implements OnInit {
       related: {
         name: 'Fulano de tal',
         action: 'like'
+      },
+      actor: {
+        name: 'John Dere',
+        description: 'Fullstack Developer',
+        hours: 3
       }
     },
     {
       related: {
         name: 'Outro Fulano',
         action: 'comments'
+      },
+      actor: {
+        name: 'Luke Skywalker',
+        description: 'Recruiting, Global Product at Facebook',
+        hours: 11
       }
     },
     {
-      related: null
+      related: null,
+      actor: {
+        name: 'Mary Jane',
+        description: 'QA at Google',
+        hours: 11
+      }
     }
   ]
 
