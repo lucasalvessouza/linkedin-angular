98f9889e74da517c2269d8ad6fb10be24f910d3e
Lucas Alves <lucasalves.s@outlook.com>
2020-09-07 22:48:06 -0300

    right side card

--- src/app/home/feed/feed.component.ts
+++ src/app/home/feed/feed.component.ts
@@ -16,9 +16,16 @@ export class FeedComponent implements OnInit {
       actor: {
         name: 'John Dere',
         description: 'Fullstack Developer',
+        avatar: 'assets/img/posts/avatar1.png',
         time: '3h'
       },
-      reactions: ['love', 'celebrate']
+      reactions: ['love', 'celebrate'],
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porttitor feugiat dolor,
+      non imperdiet risus porttitor ut. Etiam non turpis quis justo vulputate blandit.
+      Proin facilisis ornare suscipit. Sed vel lorem sodales, rhoncus odio facilisis, elementum elit.
+      Nulla egestas egestas interdum. In tellus ante, placerat non velit luctus, ullamcorper porttitor nibh
+      `,
+      image: 'assets/img/posts/post1.jpeg'
     },
     {
       related: {
@@ -27,19 +34,30 @@ export class FeedComponent implements OnInit {
       },
       actor: {
         name: 'Luke Skywalker',
+        avatar: 'assets/img/posts/avatar2.png',
         description: 'Recruiting, Global Product at Facebook',
         time: '11h'
       },
-      reactions: ['like', 'love', 'interesting']
+      reactions: ['like', 'love', 'interesting'],
+      text: `Curabitur ornare, metus at fringilla faucibus, diam lectus tincidunt ex, et euismod lacus massa at est.
+      Donec non laoreet nulla, et commodo quam. Phasellus ut turpis et erat maximus lobortis.
+      Proin feugiat dignissim nibh, in porttitor mi consequat nec. Vivamus auctor elit non tempus luctus.
+      `,
+      image: 'assets/img/posts/post2.jpg'
     },
     {
       related: null,
       actor: {
         name: 'Mary Jane',
+        avatar: 'assets/img/posts/avatar3.png',
         description: 'QA at Google',
         time: '11h'
       },
-      reactions: ['love']
+      reactions: ['love'],
+      text: `Suspendisse vestibulum fringilla pharetra. Pellentesque vel sapien urna. Integer molestie diam vel ultricies sodales.
+      In sit amet nulla sed felis cursus sagittis.
+      `,
+      image: 'assets/img/posts/post3.jpg'
     }
   ]
 
